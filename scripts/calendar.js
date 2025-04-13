6eaf759fef3b892b537183406dfe27314ddb66b1
ramcan7 <m.ramos@pucp.edu.pe>
2025-04-13 17:47:14 -0500

    Week calendar styling done

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,13 +1,21 @@
 export function initCalendar() {
     const monthCalendarElement = document.querySelector("[data-month-calendar]");
+    const weekCalendarElement = document.querySelector("[data-week-calendar]");
+    const dayCalendarElement = document.querySelector("[data-day-calendar]");
 
     document.addEventListener("view-change", 
         (event) => {
             const selectedView = event.detail.view;
             if(selectedView === "month") {
                 monthCalendarElement.style.display = "flex";
+                weekCalendarElement.style.display = "none";
+            }
+            else if(selectedView === "week") {
+                weekCalendarElement.style.display = "flex";
+                monthCalendarElement.style.display = "none";
             }
             else {
+                weekCalendarElement.style.display = "none";
                 monthCalendarElement.style.display = "none";
             }
         }
